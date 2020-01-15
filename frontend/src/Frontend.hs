36420c993e71ca4eecde1965f4a61ae747efabd1
Ryan Trinkle <ryan@trinkle.org>
2020-01-15 15:03:27 -0500

    Create hydration failure

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,36 +1,23 @@
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
-
 module Frontend where
 
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
 
 import Obelisk.Frontend
-import Obelisk.Configs
 import Obelisk.Route
-import Obelisk.Generated.Static
 
 import Reflex.Dom.Core
 
-import Common.Api
 import Common.Route
 
 
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
-  { _frontend_head = do
-      el "title" $ text "Obelisk Minimal Example"
-      elAttr "link" ("href" =: static @"main.css" <> "type" =: "text/css" <> "rel" =: "stylesheet") blank
+  { _frontend_head = pure ()
   , _frontend_body = do
-      el "h1" $ text "Welcome to Obelisk!"
-      el "p" $ text $ T.pack commonStuff
-      elAttr "img" ("src" =: static @"obelisk.jpg") blank
-      el "div" $ do
-        exampleConfig <- getConfig "common/example"
-        case exampleConfig of
-          Nothing -> text "No config file found in config/common/example"
-          Just s -> text $ T.decodeUtf8 s
-      return ()
+      let svgRootCfg = def
+            & elementConfig_namespace .~ Just "http://www.w3.org/2000/svg"
+            & elementConfig_initialAttributes .~ ("width" =: "100%" <> "height" =: "100%" <> "viewBox" =: "0 0 1536 2048")
+      _ <- element "svg" svgRootCfg $ do
+        dyn_ $ blank <$ pure ()
+      pure ()
   }
